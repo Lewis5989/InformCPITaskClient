f326c1f1e05b929888a622fdf8d8963d37d094af
Lewis5989 <lewis_ac@hotmail.co.uk>
2022-07-23 14:14:48 +0100

    Loaded contacts into ContactsTable

--- informcpitaskclient/src/Component/ContactsTable.js
+++ informcpitaskclient/src/Component/ContactsTable.js
@@ -1,21 +1,60 @@
+import { Component } from 'react';
 import './ContactsTable.css';
+import React from 'react';
+import { GetContactsForUser } from '../Service/ContactsService';
 
-function ContactsTable() {
-  return (
-    <div className="ContactsTable">
-        <h1>Contacts</h1>
+export class ContactsTable extends Component {
+    userId = 1;
+    constructor(props) {
+        super(props);
+        this.state = { contacts: [], loading: true };
+    }
+
+    render(){
+        let content = this.state.loading ? <div>loading..</div>: this.renderContactsTable(this.state.contacts);
+        return (
+            <div className="ContactsTable">
+                <h1>Contacts</h1>
+                {content}
+        </div>
+        );
+    };
+
+    async loadContactData(userId){
+        let data = await GetContactsForUser(userId);
+        this.setState({ contacts: data, loading: false });
+    };   
+
+    componentDidMount(){
+        this.loadContactData(this.userId);
+    };
+    
+    renderContactsTable(contacts){
+        return(
             <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th> </th>
-                </tr>
-                
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th> </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts.map(contact =>  
+                        <tr>
+                            <th>{contact.contactName}</th>
+                            <th>{contact.email}</th>
+                            <th>{contact.phoneNumber}</th>
+                            <th> </th>
+                        </tr>
+                    )}
+                </tbody>
             </table>
-        
-    </div>
-  );
+        );  
+    }
 }
+
+
 
 export default ContactsTable;
