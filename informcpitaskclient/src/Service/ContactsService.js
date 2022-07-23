e909c2523b249c0797fe7300e61123d9b94d78a7
Lewis5989 <lewis_ac@hotmail.co.uk>
2022-07-23 19:27:43 +0100

    added add or Update method to ContactsService

--- informcpitaskclient/src/Service/ContactsService.js
+++ informcpitaskclient/src/Service/ContactsService.js
@@ -3,10 +3,27 @@ export async function GetContactsForUser(userId){
         return await response.json();
     }
 
-export function CreateContact(contact){
-    if(contact.userId != null){
-        
+export async function AddOrUpdateContact(contact){
+    if(contact.userId != null && contact.id != null){
+        if(contact.id < 1){
+            var response = await fetch('https://localhost:7213/api/contacts/', {
+                method: 'POST', 
+                headers:{'Content-Type':'application/json'},
+                mode: 'cors', 
+                body: JSON.stringify(contact)
+            })
+            return await response.json();
+        }
+        response = await fetch('https://localhost:7213/api/contacts/', {
+            method: 'Put', 
+            mode: 'cors', 
+            headers:{'Content-Type':'application/json'},
+            body: JSON.stringify(contact)
+        })
+        return await response.json();
+
     }
 }
+
 
 
